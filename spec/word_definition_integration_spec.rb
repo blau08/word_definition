86e8947ff3c610f11af2bc366d8f3e093e647f44
Brian Lau <blau08@gmail.com>
2015-08-14 16:42:10 -0700

    Pass some integration specs.

--- spec/word_definition_integration_spec.rb
+++ spec/word_definition_integration_spec.rb
@@ -7,23 +7,14 @@ describe('the add word path', {:type => :feature}) do
   it('adds a word to the list') do
     visit('/')
     fill_in('word', :with => 'dog')
-    click_button('Add Contact')
-    expect(page).to have_content('dog')
-  end
-
-  it('brings user to word-specific page') do
-    visit('/')
-    fill_in('first_name', :with => 'dog')
     click_button('Add Word')
-    click_link('dog')
     expect(page).to have_content('dog')
   end
 
   it('adds a definition to word') do
-    visit('/')
-    fill_in('first_name', :with => 'dog')
-    click_button('Add definition')
-    click_link('Nancy Pelosi')
+    visit('/new_word')
+    fill_in('definition', :with => 'animal')
+    click_button('Add Definition')
     expect(page).to have_content('animal')
   end
 end
